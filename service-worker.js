ff627e6901416e1de9c362bb80838adb3e2a32e9
Juantuporaqui <juantuporaqui@yahoo.es>
2024-09-21 19:03:48 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,29 @@
-const cacheName = 'calendario-cache-v1';
-const filesToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/icon-192.png',
-    '/icon-512.png'
+const CACHE_NAME = 'calendario-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
 });
