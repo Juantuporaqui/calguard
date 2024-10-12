12f803111c3976b48642586ffa77aa56cd87a939
Juantuporaqui <juantuporaqui@yahoo.es>
2024-10-12 09:17:07 +0200

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -376,6 +376,9 @@ function markWeekAsGuardia(dayElement) {
         diasLibresUsados: []                 // Inicialmente no se han usado días libres
     });
 
+    // Aquí añadimos el log para verificar el estado de guardiasRealizadas
+    console.log('Guardias registradas:', guardiasRealizadas);
+    
     // Incrementa el contador de días libres
     daysLibres += diasPorGuardia;
     updateCounter();
